8c13836b55beafadfb14aeae092c284abb0f18e7
Brian6330 <brian@schweigler.org>
2019-02-20 15:07:08 +0100

    Initial commit

---
+++ Programs/cmake-build-debug/CMakeFiles/Programs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programs.dir/hello.c.obj"
+  "Programs.pdb"
+  "Programs.exe"
+  "Programs.exe.manifest"
+  "libPrograms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Programs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programs/hello.c
@@ -0,0 +1,13 @@
+/* hello.c -- prints "Hello World"
+ *
+ * Default compile method
+ *
+ * Run with no input parameters
+ */
+#include <stdio.h>
+
+int main(void) {
+// \n acts as a new line statement
+    printf("Hello World\n");
+    return 0;
+}
