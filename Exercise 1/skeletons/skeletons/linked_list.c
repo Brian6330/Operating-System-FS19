a72e15bd4b95b1b4d12d4864834440600631f460
Brian6330 <brian@schweigler.org>
2019-02-25 16:26:28 +0100

    Linked_list solution that is yet to be tested

--- Exercise 1/skeletons/skeletons/linked_list.c
+++ Exercise 1/skeletons/skeletons/linked_list.c
@@ -35,17 +35,66 @@ static void xfree(void *ptr)
 
 static struct elem *init_list(size_t len)
 {
-	/* ... */
+	struct elem* head;
+	struct elem* prev;
+
+	int k;
+
+	if (len < 1) {
+		panic("Length must be bigger or eqaul to 1!\n");
+	}
+
+	// First element
+	head = (struct elem*) xmalloc(sizeof(struct elem));
+	head->pos = 0;
+	head->next = NULL;
+
+	// Keep `head` as return value, but need a floating reference to previous item
+	prev = head; //TODO Switch this around out of curiosity
+
+	// Starting at entry 1, since we need to skip head
+	for (k = 1; k < len; k++) {
+		struct elem* cur = xmalloc(sizeof(struct elem));
+		cur->pos = k;
+		cur->next = NULL;
+		prev->next = cur;
+		prev = cur;
+	}
+
+	// Link last item to first
+	(*prev).next = head;
+
+	return head;
 }
 
 static void clean_list(struct elem *head, size_t len)
 {
-	/* ... */
+	struct elem* cur = head;
+
+	int k;
+
+	for (k = 0; k < len; k++) {
+		struct elem* next = cur->next;
+		xfree(cur);
+		cur = next;
+	}
 }
 
 static void traverse_list(struct elem *head, int times)
 {
-	/* ... */
+	int startIndex = head->pos;
+
+	struct elem* cur = head;
+	int iteration = 1;
+
+	while (times > 0) {
+		printf("[%i] -> %i\n", iteration, cur->pos);
+		cur = cur->next;
+		if (startIndex == cur->pos) {
+			times--;
+		}
+		iteration++;
+	}
 }
 
 int main(void)
@@ -53,7 +102,10 @@ int main(void)
 	struct elem *head = NULL;
 	size_t len = 10;
 
-	/* ... */
+	//1. Set Head | 2. iterate through list twice | 3. empty the list
+	head = init_list(len);
+	traverse_list(head, 2);
+	clean_list(head, len);
 
 	return 0;
 }
