3c332e729038613de97a56fc5131069bc3d24ec3
Brian6330 <brian@schweigler.org>
2019-04-15 13:28:23 +0200

    Untested Exercise 4

--- Exercise 4/skeletons/memory.c
+++ Exercise 4/skeletons/memory.c
@@ -2,24 +2,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <math.h>
 
 /* TODO: Insert System Type here
  * System used: e.g. Linux MyDistro 32bit x86
  */
 
-int calculate_page (/* TODO: Insert correct data type */ address) {
-	/* TODO: Implement */
+#define PAGE_SIZE 4096;
+
+int calculate_page (uint32_t address) {
+	return address / PAGE_SIZE;
 }
 
-int calculate_offset (/* TODO: Insert correct data type */ address , int page) {
-	/* TODO: Implement */
+int calculate_offset (uint32_t address , int page) {
+	return address % PAGE_SIZE;
 }
 
 int main(int argc, char *argv[])
 {
 	long long_input;
-	/* TODO: Insert correct data type */ address;
+	uint32_t address;
 	int offset , page;
 
 	if (argc != 2) {
@@ -27,16 +30,21 @@ int main(int argc, char *argv[])
 		return -1;
 	} else {
 		long_input = atol(argv[1]);
-		long supremum = pow(2 , 8*sizeof(/* TODO: Insert correct data type */));
+		long supremum = pow(2 , 8*sizeof(uint32_t));
 		if (long_input < 0 || long_input >= supremum) {
 			printf("You must enter a valid 32bit address as parameter!\n");
 			return -1;
 		} else {
-			address = (/* TODO: Insert correct data type */) long_input;
+			address = (uint32_t) long_input;
 		}
 	}
 
-	/* TODO: calculate values and generate correct output */
+	page = calculate_page(address);
+	offset = calculate_offset(address);
+
+	printf("The address %u contains:\n", address);
+	printf("Page number: %i\n", page);
+	printf("Offset: %i\n", offset);
 
 	return 0;
 }
